4417fba4087e5c97c5bb857adb3adf896b096ec2
Jonathan Oakes <jonathanmartynoakes@gmail.com>
2022-05-25 15:27:17 +0100

    added logic to render add to order button for first two categories

--- src/components/add-to-order-button/add-to-order-button.component.jsx
+++ src/components/add-to-order-button/add-to-order-button.component.jsx
@@ -1,21 +1,34 @@
-import useSizeSaucesAndQuantityCheck from "./checks/use-size-sauce-quantity-check";
-import useSaucesAndQuantityCheck from "./checks/use-sauce-quantity-check";
-import useNoOptionsCheck from "./checks/use-no-options-check";
+import useburgersCategoryAndQuantityCheck from "./checks/use-burgers-category-and-quantity-check";
+import useNoOptionsAndQuantityCheck from "./checks/use-no-options-and-quantity-check";
+import useSizeSaltAndVinegarAndQuantityCheck from "./checks/use-size-salt-and-vinegar-and-quantity-check";
+import useSizeAndQuantityCheck from "./checks/use-size-and-quantity-check.component";
+import useSaltAndVinegarAndQuantityCheck from "./checks/use-salt-and-vinegar-and-quantity-check";
+import useSaltAndVinegarDrinkAndQuantityCheck from "./checks/use-salt-and-vinegar-drink-and-quantity-check";
 
 import AddToOrderButtonError from "./add-to-order-button-error";
 
 import { AddToOrderButtonContainer } from "./add-to-order-button.styles";
 
 const AddToOrderButton = ({ children, ...props }) => {
-  const { sizeSaucesAndQuantityCheck } = useSizeSaucesAndQuantityCheck();
-  const { sauceQuantityCheck } = useSaucesAndQuantityCheck();
-  const { noOptionsCheck } = useNoOptionsCheck();
+  const { burgersCategoryAndQuantityCheck } =
+    useburgersCategoryAndQuantityCheck();
+  const { noOptionsAndQuantityCheck } = useNoOptionsAndQuantityCheck();
+  const { sizeSaltAndVinegarAndQuantityCheck } =
+    useSizeSaltAndVinegarAndQuantityCheck();
+  const { sizeAndQuantityCheck } = useSizeAndQuantityCheck();
+  const { saltAndVinegarAndQuantityCheck } =
+    useSaltAndVinegarAndQuantityCheck();
+  const { saltAndVinegarDrinkAndQuantityCheck } =
+    useSaltAndVinegarDrinkAndQuantityCheck();
 
   return (
     <>
-      {sizeSaucesAndQuantityCheck() ||
-      sauceQuantityCheck() ||
-      noOptionsCheck() ? (
+      {burgersCategoryAndQuantityCheck() ||
+      noOptionsAndQuantityCheck() ||
+      sizeSaltAndVinegarAndQuantityCheck() ||
+      sizeAndQuantityCheck() ||
+      saltAndVinegarAndQuantityCheck() ||
+      saltAndVinegarDrinkAndQuantityCheck() ? (
         <AddToOrderButtonContainer {...props}>
           {children}
         </AddToOrderButtonContainer>
